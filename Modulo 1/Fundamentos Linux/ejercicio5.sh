97b284c07ae6dd35cf93d778501a33ae6292be86
javi <jcmorente94@gmail.com>
2023-09-19 18:40:02 +0200

    change indent

--- Modulo 1/Fundamentos Linux/ejercicio5.sh
+++ Modulo 1/Fundamentos Linux/ejercicio5.sh
@@ -4,19 +4,19 @@ url=$1
 word=$2
 
 if [ "$#" != 2 ]; then
-    echo "Se necesitan unicamente dos parametros para ejecutar este script"
+  echo "Se necesitan unicamente dos parametros para ejecutar este script"
 else
-    curl -s $url >file.txt
-    count=$(grep -ac $word file.txt)
-    line=$(grep -anm1 $word file.txt | cut -d ":" -f 1)
+  curl -s $url >file.txt
+  count=$(grep -ac $word file.txt)
+  line=$(grep -anm1 $word file.txt | cut -d ":" -f 1)
 
-    if [ $count == 0 ]; then
-        echo "No se ha encontrado la palabra \"$word\""
-    elif [ $count == 1 ]; then
-        echo "La palabra \"$word\" aparece $count vez"
-        echo "Aparece unicamente en la linea $line"
-    else
-        echo "La palabra \"$word\" aparece $count veces"
-        echo "Aparece por primera vez en la linea $line"
-    fi
+  if [ $count == 0 ]; then
+    echo "No se ha encontrado la palabra \"$word\""
+  elif [ $count == 1 ]; then
+    echo "La palabra \"$word\" aparece $count vez"
+    echo "Aparece unicamente en la linea $line"
+  else
+    echo "La palabra \"$word\" aparece $count veces"
+    echo "Aparece por primera vez en la linea $line"
+  fi
 fi
