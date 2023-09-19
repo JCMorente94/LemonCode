154c01e60e1b8d688e519a506e45e3df8df6d702
javi <jcmorente94@gmail.com>
2023-09-19 18:29:33 +0200

    format files

--- Modulo 1/Fundamentos Linux/ejercicio3.sh
+++ Modulo 1/Fundamentos Linux/ejercicio3.sh
@@ -9,10 +9,9 @@ fi
 mkdir -p foo/dummy
 mkdir -p foo/empty
 
-echo $text > foo/dummy/file1.txt
+echo $text >foo/dummy/file1.txt
 touch foo/empty/file2.txt
 
-cat foo/dummy/file1.txt > foo/dummy/file2.txt
+cat foo/dummy/file1.txt >foo/dummy/file2.txt
 
 mv foo/dummy/file2.txt foo/empty/file2.txt
-	
